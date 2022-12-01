79cf6f75f64a57f5b23b986ab01ac582e154287a
Tobin Cooney <tobin@tobincooney.com>
2022-12-01 17:14:57 -0500

    RSS feed, siteinfo.txt, date format.

--- blog.sh
+++ blog.sh
@@ -2,9 +2,14 @@
 
 mkdir blog
 
+builddate=$( date +%a,\ %d\ %b\ %Y )
+
 head=$( cat head.htm )
 foot=$( cat foot.htm )
 deck=$( cat deck.htm )
+url=$( head -1 siteinfo.txt )
+sitename=$( head -2 siteinfo.txt | tail -1 )
+sitedesc=$( tail -1 siteinfo.txt )
 
 echo "sorting index..."
 index=()
@@ -20,6 +25,7 @@ echo ". sorted	${index[@]}"
 
 echo "building..."
 cards=""
+rssstack=""
 for i in "${index[@]}"
 do
 	items=($i)
@@ -35,6 +41,15 @@ do
 	img=$( echo "$main" | grep "class=\"titular\"" | sed 's/<img/<img style="width:200px;" width="200"/' )
 	page="$spechead<h1>$title</h1><p><code>$date</code></p>$main$foot"
 	echo "$page" > blog/$filename
+	rssstack="<item>
+  <title>$title</title>
+  <link>$url/blog/$filename</link>
+  <pubDate>$date</pubDate>
+  <description>
+<![CDATA[$main]]>
+  </description>
+</item>
+$rssstack"
 
 	cards="<div class='minipost'>
 	<h2><a href='$filename'>$title</a></h2>
@@ -47,6 +62,19 @@ do
 <br>
 $cards"
 done
+
+echo "writing rss.xml..."
+rss="<?xml version='1.0' encoding='UTF-8' ?>
+<rss version='2.0'>
+<channel>
+<title>$sitename</title>
+<link>$url</link>
+<description>$sitedesc</description>
+<lastBuildDate>$builddate</lastBuildDate>
+$rssstack
+</channel>
+</rss>"
+echo "$rss" > blog/rss.xml
 
 echo "creating deck page..."
 title=$( echo "$deck" | head -1 )
